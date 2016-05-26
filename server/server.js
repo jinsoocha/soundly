95b51f3572d2a0a1626998e6aba29af5551ce4bb
Sehoon Park <milkpatron@me.com>
2016-05-26 15:48:54 -0700

    Set up and load server modules

--- server/server.js
+++ server/server.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var morgan = require('morgan');
+var path = require('path');
+
+app.use(morgan('combined'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/../compiled'));
 app.get('/', function(req, res) {
   res.send('Happening here.');
+});
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/../compiled/index.html');
 });
 
 // connect to mongoDB
