feb27d057c4b92a01978f7ce0514fcb97f6f4c1b
Jinsoo Cha <jinscha@indiana.edu>
2016-05-26 17:17:11 -0700

    sending the searchKeyword to the server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var morgan = require('morgan');
 var db = require('./db-config');
 var User = require('./models/User.js');
 var Song = require('./models/Song.js');
+var bodyParser = require('body-parser');
+// sending the searchKeyword to the server
 
 app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -40,3 +41,32 @@ app.get('/songs', function(req, res) {
 var port = process.env.PORT || 4568;
 app.listen(port);
 console.log('Music happens on port: ' + port);
+
+var data;
+
+app.post('/server',function(req,res) {
+	data = req.body;
+  return res.send({statusCode: 200, status: 'OK', data: req.body})
+
+	// return SC.get('/tracks', {
+ //  			q: example,
+ //  			streamable: true
+	// 		}).bind(this)
+	// 		.then(function(tracks) {
+	//   		console.log(tracks)
+	// 		});
+}); 
+
+var headers = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10 // Seconds.
+};
+
+
+app.get('/server', function(req, res) {
+	res.writeHead(200, headers);
+  res.end(JSON.stringify(data));
+});
+//sending the searchKeyword to the server
