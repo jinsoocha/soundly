0a3c5c805c642ec4ac34df079c172831935a27fd
Jinsoo Cha <jinscha@indiana.edu>
2016-05-27 16:21:13 -0700

    install bower instead of cdn

--- client/index.jsx
+++ client/index.jsx
@@ -4,7 +4,7 @@
 
 ReactDOM.render((
 	<Router>
-  	<Route path="/" component={App} />
+  	<Route path="/" component={App}>
     	<Route path="queue" component={QueueView} />
 			<Route path="search" component={SearchView} />	
 		</Route>	
