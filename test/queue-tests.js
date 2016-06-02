d1af87453892a7101d12fef5c07482a86f359c6d
Bill Ramsey <billramsey@gmail.com>
2016-06-02 11:01:59 -0700

    add .eslint to tests

---
+++ test/queue-tests.js
@@ -0,0 +1,15 @@
+var assert = require('assert');
+var queue = require('../server/routes/queue.js');
+
+
+describe('queues', function() {
+
+  before(function() {
+    // runs before all tests in this block
+
+  });
+
+  it('should catch promised error when trying to remove song from empty queue', function() {
+
+  });
+});
