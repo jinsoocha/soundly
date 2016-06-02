dc21c22dfb45ecd84234dd923eff6152de29c7c6
Bill Ramsey <billramsey@gmail.com>
2016-06-02 16:13:40 -0700

    Implement queue algorithm and tests

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const users = require('./routes/users.js');
 const songs = require('./routes/songs.js');
 const soundcloud = require('./routes/soundcloud.js');
+const queue = require('./routes/queue.js');
 // //attach http server to the express app
 const http = require('http').Server(app);
 // //attach socket io to http server
@@ -28,25 +29,44 @@ app.post('/server', soundcloud.get);
 // when you go to localhost:4568/server you will see the data the server is holding if any.
 app.get('/server', soundcloud.server);
 
+//  Queue Routes
+//  Get all songs in queue
+app.get('/api/queue/getQueue', queue.getSongQueue);
+//  Remove first song from queue since it finished playing
+app.post('/api/queue/songFinished', queue.firstSongFinished);
+//  Passes song object to add to queue at end.
+app.post('/api/queue/addSong', queue.addSongToQueue);
+//  Passes array index of song to increase in rank.
+app.post('/api/queue/increaseRank', queue.increaseSongRanking);
+//  Passes array index of song to decrease in rank.
+app.post('/api/queue/decreaseRank', queue.decreaseRank);
+//  Passes array index of song to move up in queue.
+app.post('/api/queue/moveUpInQueue', queue.moveUpInQueue);
+//  Passes array index of song to move down in queue.
+app.post('/api/queue/moveDownInQueue', queue.moveDownInQueue);
+//  Passes array index of song to remove from queue
+app.post('/api/queue/removeSong', queue.removeSongFromQueue);
+
+
 // song queue -- will moudlarize //
 // ********************************** //
-const headers = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10, // Seconds.
-};
-const songsQueue = [];
-app.post('/queue', function(req, res) {
-  songsQueue.push(req.body);
-  console.log('$$$$reqbody',req.body, '$$$$songdqueue', songsQueue);
-  return res.send({ statusCode: 200, status: 'OK', data: songsQueue });
-});
+// const headers = {
+//   'access-control-allow-origin': '*',
+//   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+//   'access-control-allow-headers': 'content-type, accept',
+//   'access-control-max-age': 10, // Seconds.
+// };
+// const songsQueue = [];
+// app.post('/queue', function(req, res) {
+//   songsQueue.push(req.body);
+//   console.log('$$$$reqbody',req.body, '$$$$songdqueue', songsQueue);
+//   return res.send({ statusCode: 200, status: 'OK', data: songsQueue });
+// });
 
-app.get('/queue', function(req, res) {
-  res.writeHead(200, headers);
-  res.end(JSON.stringify(songsQueue));
-});
+// app.get('/queue', function(req, res) {
+//   res.writeHead(200, headers);
+//   res.end(JSON.stringify(songsQueue));
+// });
 // ********************************** //
 
 const port = process.env.PORT || 4568;
