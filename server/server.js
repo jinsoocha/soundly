163afb161e35328d69366de0cd013f5a9cc46c0f
aaronwbrown <aaron.w.brown13@gmail.com>
2016-06-02 18:38:32 -0700

    Change server to match.

--- server/server.js
+++ server/server.js
@@ -10,39 +10,9 @@ app.use(express.static(__dirname + '/../compiled'));
 
 require('./routes/middleware.js')(app, express);
 
-<<<<<<< HEAD
-app.get('/songs', songs.find);
-// when the client posts the search input, server receives and makes an api call to
-// get the corresponding tracks
-app.post('/server', soundcloud.get);
-// when you go to localhost:4568/server you will see the data the server is holding if any.
-app.get('/server', soundcloud.server);
-// song queue -- will modularize //
-//  Queue Routes
-//  Get all songs in queue
-app.get('/api/queue/getQueue', queue.getSongQueue);
-//  Remove first song from queue since it finished playing
-app.post('/api/queue/songFinished', queue.firstSongFinished);
-//  Passes song object to add to queue at end.
-app.post('/api/queue/addSong', queue.addSongToQueue);
-//  Passes array index of song to increase in rank.
-app.post('/api/queue/increaseRank', queue.increaseSongRanking);
-//  Passes array index of song to decrease in rank.
-app.post('/api/queue/decreaseRank', queue.decreaseSongRanking);
-//  Passes array index of song to move up in queue.
-app.post('/api/queue/moveUpInQueue', queue.moveUpInQueue);
-//  Passes array index of song to move down in queue.
-app.post('/api/queue/moveDownInQueue', queue.moveDownInQueue);
-//  Passes array index of song to remove from queue
-app.post('/api/queue/removeSong', queue.removeSongFromQueue);
-
-
-
-
-// ********************************** //
 require('./routes/routes.js')(app, express);
-require('./routes/socketio.js')(app, express, http, io);
 
+require('./routes/socketio.js')(app, express, http, io);
 
 const port = process.env.PORT || 4568;
 
